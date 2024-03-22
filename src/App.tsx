d3e199023e419a84a67489f453018a0953c5ef22
Bernhard Ricardo Putranto <Bernhard.R.Putranto@stud.h-da.de>
2024-03-22 09:18:25 +0100

    Add routing and theme to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import '@mantine/core/styles.css';
-
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { routes } from './constants';
+import { Shell } from './common/Shell';
 import { MantineProvider } from '@mantine/core';
+import { theme } from './theme';
 
 function App() {
-  return <MantineProvider></MantineProvider>;
+  return (
+    <MantineProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Shell />}>
+          {routes.map(({ path, Element }) => (
+            <Route key={path} path={path} element={<Element />} />
+          ))}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </MantineProvider>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { BASE_PATH } from './constants.ts';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={BASE_PATH}>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
